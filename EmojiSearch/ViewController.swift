677d76331a409c0b94b9fb3732752873ae5cd166
Ariana DeJesus <Ariana@minitests-mac-75.local>
2019-02-23 08:17:35 -0800

    Initial Commit

---
+++ EmojiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiSearch
+//
+//  Created by Ariana DeJesus on 2/23/19.
+//  Copyright © 2019 web151. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
