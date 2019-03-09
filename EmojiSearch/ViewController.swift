109495f1c4805e1a8af29990529eeae816143a40
Ariana DeJesus <Ariana@minitests-mac-75.local>
2019-03-08 17:49:41 -0800

    final refactor of dropdown selection code, edit documentation

--- EmojiSearch/ViewController.swift
+++ EmojiSearch/ViewController.swift
@@ -10,20 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var dropDownStack: UIStackView!
-    /* tag each button in Interface Builder
-     1-4 : happy
-     5-8 : sad
-     9- 12: tired
-     13 - 16 : love
-     17 - 20 : angry
-     */
+    @IBOutlet weak var dropDownStack: UIStackView! // create this outlet to bring this view in front of showAll button
     
     @IBOutlet weak var findEmojiBtn: UIButton!
     @IBOutlet weak var tblView: UITableView!
     @IBOutlet weak var showAllBtn: UIButton!
     
-   // create collection outlets for each group of emoji
+    // create collection outlets for each group of emoji
     @IBOutlet var happyEmojiBtns: [customizeButton]!
     @IBOutlet var sadEmojiBtns: [customizeButton]!
     @IBOutlet var tiredEmojiBtns: [customizeButton]!
@@ -32,16 +25,16 @@ class ViewController: UIViewController {
     
     var emotionList = ["Happy", "Sad", "Tired", "Love", "Angry"] // use for drop down list
     
-    // create dictionary to hold button arrays
-    var emojiDict = [String: [UIButton]]()
+    var emojiDict = [String: [UIButton]]()// dictionary to hold button outlet collections
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tblView.isHidden = true // hide dropdown list on load
-        self.view.bringSubviewToFront(dropDownStack)// bring drop down in front of showAll button
         
-        // set dictionary values to button collections
+        tblView.isHidden = true // hide dropdown list on load
+        self.view.bringSubviewToFront(dropDownStack)// bring dropdown in front of showAll button
+        
+        // set dictionary values
         emojiDict["Happy"] = happyEmojiBtns
         emojiDict["Sad"] = sadEmojiBtns
         emojiDict["Tired"] = tiredEmojiBtns
@@ -49,8 +42,7 @@ class ViewController: UIViewController {
         emojiDict["Angry"] = angryEmojiBtns
     }
     
-    // reveal/hide dropdown list when user clicks find emoji by group button
-    @IBAction func onClickFindEmojiBtn(_ sender: Any) {
+    @IBAction func onClickFindEmojiBtn(_ sender: Any) {// reveal/hide dropdown list when user clicks find emoji by group button
         if tblView.isHidden {
             animate(toggle: true)
         } else {
@@ -58,10 +50,9 @@ class ViewController: UIViewController {
         }
     }
     
-    // create function to toggle the dropdown
     func animate(toggle: Bool) {
         if toggle { // if toggle is true (table is invisible), set to false (unhide table)
-            UIView.animate(withDuration: 0.3) {
+            UIView.animate(withDuration: 0.3) {// animate creates a short effect when table drops in and out, looks smoother
                 self.tblView.isHidden = false
             }
         } else { // if false, set toggle to true (hide table)
@@ -69,93 +60,36 @@ class ViewController: UIViewController {
                 self.tblView.isHidden = true
             }
         }
-    }// end function animate
+    }// end animate function
     
-    func hideEmojis(by selected: String){
-        // make sure all emojis are showing before hiding
-        showAllEmojis(showAllBtn)
+    func displayEmojiGroup(by selected: String){
+        showAllEmojis(showAllBtn)// make sure all emojis are showing before hiding
         
-        
-        for (name, group) in emojiDict {// access dictionary
-            switch selected {
-            case "Happy":
-                if name != "Happy" { // access specific groups in dict
-                    for button in group { // access each button in array and hide
-                        button.isHidden = true
-                    }
-                }
-            default:
-                print("Something went wrong.")
-            }// end switch
-        }// end for loop
-        
-        /*
-        // access dictionary key by selected, hide buttons not selected
-        switch selected {
-        case "Happy":
-            for (name, group) in emojiDict { // get key/value pair from dict
-                if name != "Happy" { // access specific groups in dict
-                    for button in group { // access each button in array and hide
-                        button.isHidden = true
-                    }
-                }
+        for (name, collection) in emojiDict { // access dictionary
+            for button in collection where name != selected { // access each button in collection except for choice selected by user
+                button.isHidden = true
             }
-        case "Sad":
-            for (name, group) in emojiDict {
-                if name != "Sad" {
-                    for button in group {
-                        button.isHidden = true
-                    }
-                }
-            }
-        case "Tired":
-            for (name, group) in emojiDict {
-                if name != "Tired" {
-                    for button in group {
-                        button.isHidden = true
-                    }
-                }
-            }
-        case "Love":
-            for (name, group) in emojiDict {
-                if name != "Love" {
-                    for button in group {
-                        button.isHidden = true
-                    }
-                }
-            }
-        case "Angry":
-            for (name, group) in emojiDict {
-                if name != "Angry" {
-                    for button in group {
-                        button.isHidden = true
-                    }
-                }
-            }
-        default:
-            print("something went wrong")
         }
-    */
-    }// end function hideEmojis
+    }// end displayEmojiGroup function
     
-    // show all buttons again
     @IBAction func showAllEmojis(_ sender: UIButton) {
-        for group in emojiDict.values { // loop through dictionary's collections
-            for button in group { // for each button in collection, unhide
+        for collection in emojiDict.values { // loop through dictionary's collections
+            for button in collection { // unhide buttons
                 button.isHidden = false
             }
+        }
     }// end showAll function
     
-}
 }// end class ViewController
+
 
 // create extension to fill the dropdown table view
 // need to connect to viewController delegate/datasource first by ctrl dragging from tableview to viewController
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
-    // get list of emotions and put into rows
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return emotionList.count // return number of items in list to account for rows
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {// get list of emotions and put into rows
+        return emotionList.count // return number of items in list to account for rows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -164,10 +98,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
-    // do stuff when item is selected
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selected = self.emotionList[indexPath.row]// get selected item into as a string
-        hideEmojis(by: selected)// send to function to display group of emojis
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {// do stuff when item from table is selected
+        let selected = self.emotionList[indexPath.row]// get selected item into a string
+        displayEmojiGroup(by: selected)// send selected to function to display specified group of emojis and hide all others
         animate(toggle: false)// close dropdown after item is selected
     }
 }
