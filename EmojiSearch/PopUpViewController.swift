e4bd8eb9a01e8d5cd9c584ef9629ba3ec2acc810
Ariana DeJesus <Ariana@minitests-mac-75.local>
2019-03-10 10:01:02 -0700

    refactored popup code, create function to get quote for popup, display quote in popup

--- EmojiSearch/PopUpViewController.swift
+++ EmojiSearch/PopUpViewController.swift
@@ -9,36 +9,18 @@
 import UIKit
 
 class PopUpViewController: UIViewController {
+    @IBOutlet weak var popupMessage: UILabel!
+    @IBOutlet weak var goBack: UIButton!
+    
+    var message = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor(white: 1, alpha: 0.3) // set transparent background
-        self.showAnimate()
+       popupMessage.text = message// display message from main view controller
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
     @IBAction func closePopup(_ sender: Any) {
-        self.removeAnimate()
+       self.dismiss(animated: true)
     }
     
-    // popup animation - courtesy of Youtuber: Seemu Apps -- video: Swift- Pop Up Tutorial
-    func showAnimate() {
-        self.view.transform = CGAffineTransform(scaleX: 1.3,y: 1.3) // makes view larger
-        self.view.alpha = 0.0
-        UIView.animate(withDuration: 0.25, animations: { // will scale down when view appears
-            self.view.alpha = 1.0
-            self.view.transform = CGAffineTransform(scaleX: 1.0,y: 1.0)
-        })
-    }
-    func removeAnimate() {
-        UIView.animate(withDuration: 0.25, animations: {
-            self.view.transform = CGAffineTransform(scaleX: 1.3,y: 1.3) // makes view larger as it removes it from screen
-            self.view.alpha = 0.0
-        }, completion: {(finished : Bool) in
-            if (finished) {
-                self.view.removeFromSuperview()
-            }
-        })
-    }
 }// end class
--- EmojiSearch/ViewController.swift
+++ EmojiSearch/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayView: UIView!
     @IBOutlet weak var displayLbl: UILabel!
     
-    // create collection outlets for each group of emoji
+    // create collection outlets for each group of emoji // will access each individual button by tag assigned in Interface Builder
     @IBOutlet var happyEmojiBtns: [customizeButton]!
     @IBOutlet var sadEmojiBtns: [customizeButton]!
     @IBOutlet var tiredEmojiBtns: [customizeButton]!
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     
     var emojiDict = [String: [UIButton]]()// dictionary to hold button outlet collections
     
-    // messages to respond to user input
+    // dictionary of messages to respond to user input
     var quoteDict = ["Happy": ["You are awesome!",
                                "Sending you a little box of sunshine to brighten your day!",
                                "You are amazing and strong and brave and wonderful. Remember that today",
@@ -57,7 +57,7 @@ class ViewController: UIViewController {
         tblView.isHidden = true // hide dropdown list on load
         self.view.bringSubviewToFront(dropDownStack)// bring dropdown in front of showAll button
         
-        // set dictionary values
+        // set dictionary values to button collections
         emojiDict["Happy"] = happyEmojiBtns
         emojiDict["Sad"] = sadEmojiBtns
         emojiDict["Tired"] = tiredEmojiBtns
@@ -73,8 +73,8 @@ class ViewController: UIViewController {
         }
     }
     
-    func animate(toggle: Bool) {
-        if toggle { // if toggle is true (table is invisible), set to false (unhide table)
+    func animate(toggle: Bool) { // animate and toggle the display of drop down list
+        if toggle { // if toggle is true (table is hidden), set to false (unhide)
             UIView.animate(withDuration: 0.3) {// animate creates a short effect when table drops in and out, looks smoother
                 self.tblView.isHidden = false
             }
@@ -85,55 +85,73 @@ class ViewController: UIViewController {
         }
     }// end animate function
     
-    func displayEmojiGroup(by selected: String){
-        showAllEmojis(showAllBtn)// make sure all emojis are showing before hiding
-        
-        for (name, collection) in emojiDict { // access dictionary
-            for button in collection where name != selected { // access each button in collection except for choice selected by user
-                button.isHidden = true
-            }
-        }
-    }// end displayEmojiGroup function
     
     @IBAction func showAllEmojis(_ sender: UIButton) {
         for collection in emojiDict.values { // loop through dictionary's collections
-            for button in collection { // unhide buttons
+            for button in collection { // unhide all buttons
                 button.isHidden = false
             }
         }
     }// end showAll function
     
-    @IBAction func onClickEmojiButtons(_ sender: customizeButton) {
+    func displayEmojiGroup(by selected: String){
+        showAllEmojis(showAllBtn)// make sure all emojis are showing before hiding
+        
+        for (name, collection) in emojiDict { // access dictionary
+            for button in collection where name != selected { // access and hide each button in collection except for choice selected by user
+                button.isHidden = true
+            }
+        }
+    }// end displayEmojiGroup function
+    
+    func getQuote(from button: Int) -> String {
+        switch button {
+        case 1...20: // happy
+            if let happyQuote = quoteDict["Happy"] { // need to unwrap since dictionary array value is optional
+                return happyQuote.randomElement()! // return a random quote from specified key
+            }
+        case 21...30: // sad
+            if let sadQuote = quoteDict["Sad"] {
+                return sadQuote.randomElement()!
+            }
+        case 31...40: // tired
+            if let tiredQuote = quoteDict["Tired"] {
+                return tiredQuote.randomElement()!
+            }
+        case 41...50: // love
+            if let loveQuote = quoteDict["Love"] {
+                return loveQuote.randomElement()!
+            }
+        case 51...60: // angry
+            if let angryQuote = quoteDict["Angry"] {
+                return angryQuote.randomElement()!
+            }
+        default:
+            return "something went wrong"
+        }
+        
+        return "Something went wrong"
+    }// end getQuote function
+    
+    @IBAction func onClickEmojiButtons(_ sender: customizeButton){
         if let emoji = sender.currentTitle { // get emoji from button clicked
             displayLbl.text = "\(emoji)" // display to screen
         }
         
-        let popUpVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopupID") as! PopUpViewController
-        self.addChild(popUpVC)// adds pop up view to main view controller
-        popUpVC.view.frame = self.view.frame
-        self.view.addSubview(popUpVC.view)
-        popUpVC.didMove(toParent: self)
-       
-              // display messages responding to user emoji
-            switch sender.tag {
-            case 1...4:
-                if let happyQuote = quoteDict["Happy"] {
-                    print(happyQuote.randomElement()!)// change this to call add quote to pop up view
-                }
-                
-            // case 21...24:
-            default:
-                print("somethting wentt wrong")
-            // case 31...34:
-            //case 41...44:
-            //case 51...54:
-            }
-    
+        let quote = getQuote(from: sender.tag)// get quote based on user click, get button id by tag assigned in IB
+        
+        self.performSegue(withIdentifier: "segue", sender: quote) // display popup // set sender to quote grabbed from getQuote function
         
     }// end onClickEmojiButtons function
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // prepares data for segue before it is displayd to user
+        let popup = segue.destination as! PopUpViewController // destination is the VC the segue is going TO // set distination TYPE to the popupVC
+        popup.message = sender as! String // set message variable to quote grabbed from getQuote function
+        
+    }
     
 }// end class ViewController
+
 
 
 // create extension to fill the dropdown table view
